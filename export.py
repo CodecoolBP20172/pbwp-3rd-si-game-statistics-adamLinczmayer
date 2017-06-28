37a595d960468acba1453fe3b4bd9acc39e3b4fa
Linczmayer Ádám <lincy.adam@gmail.com>
2017-06-28 19:52:12 +0200

    Done

--- export.py
+++ export.py
@@ -1,2 +1,17 @@
+import reports
 
-# Export functions
+
+def export_answers_from_reports(file_name, title, year, genre, export_file="reports_export.txt"):
+    file = open(export_file, "w")
+    file.write("Number of games in the file: {}\n".format(reports.count_games(file_name)))
+    file.write("The latest game is: {}\n".format(reports.get_latest(file_name)))
+    file.write("The number of games we have by the given genre: {}\n".format(reports.count_by_genre(file_name, genre))) 
+    file.write("The line number of the given game is: {}\n".format(reports.get_line_number_by_title(file_name, title)))
+    file.write("Alphabetical ordered list of the titles is: {}\n".format(reports.sort_abc(file_name)))
+    file.write("Genres are: {}\n".format(reports.get_genres(file_name)))
+    file.write("There is a game from a given year: {}\n".format(reports.decide(file_name, year)))
+    file.write("Release date of the top sold FPS game is: {}\n".format(reports.when_was_top_sold_fps(file_name)))
+    file.close()
+    return export_file
+
+export_answers_from_reports("game_stat.txt", "Terraria", 2004, "First-person shooter")
--- printing.py
+++ printing.py
@@ -1,2 +1,21 @@
+import reports
+import pprint
 
-# Printing functions
+
+def answers_prints_from_reports(file_name, year, genre, title):
+    print("Number of games in the file: {}".format(reports.count_games(file_name)))
+    print("The latest game is: {}".format(reports.get_latest(file_name)))
+    print("The number of games we have by the given genre: {}".format(reports.count_by_genre(file_name, genre)))
+    print("The line number of the given game is: {}".format(reports.get_line_number_by_title(file_name, title)))
+    pp = pprint.PrettyPrinter(indent=1, compact=False)
+    print("Alphabetical ordered list of the titles is: ")
+    pp.pprint(reports.sort_abc(file_name))
+    print("Genres are: ")
+    pp.pprint(reports.get_genres(file_name))
+    print("There is a game from a given year: {}".format(reports.decide(file_name, year)))
+    print("The release date of the top sold FPS game is: {}".format
+          (reports.when_was_top_sold_fps(file_name)))
+    return
+
+answers_prints_from_reports("game_stat.txt", 2004, "First-person shooter", "Terraria")
+
