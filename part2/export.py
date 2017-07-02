9e368726bd41a0b0ea59d4c097617a68527a67ad
Linczmayer Ádám <lincy.adam@gmail.com>
2017-07-02 14:58:05 +0200

    Done

--- part2/export.py
+++ part2/export.py
@@ -1,7 +1,7 @@
 import reports
 
 
-def export_reports_answers(file_name, title, export_file="reports_export.txt"):
+def export_reports_answers(file_name, title, export_file):
     file = open(export_file, "w")
     file.write("The most played game is: {}\n".format(reports.get_most_played(file_name)))
     file.write("The total number of the copies have been sold is: {}\n".format(reports.sum_sold(file_name)))
@@ -11,5 +11,5 @@ def export_reports_answers(file_name, title, export_file="reports_export.txt"):
     file.write("The properties of the game is: {}\n".format(reports.get_game(file_name, title)))
     file.close()
 
-export_reports_answers("game_stat.txt", "Terraria")
+export_reports_answers("game_stat.txt", "Minecraft", "reports_export.txt")
 
